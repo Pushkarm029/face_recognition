bcb81a8ff47672689cd150a02fd1f4307f227bec
Suyash Handa <87384376+Boolean-Autocrat@users.noreply.github.com>
2024-02-28 22:39:08 +0530

    [feature] adds windows support, docker files (#1)

--- compProject_FaceRecognition.py
+++ compProject_FaceRecognition.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import face_recognition
-from datetime import datetime 
+from datetime import datetime
 import csv
 import os
 from os import listdir
@@ -23,9 +23,9 @@ from os.path import isfile, join
 # 		face_encodings = face_recognition.face_encodings(rgb_img, face_locations)	#generates array of faces.
 
 # 		for (top_coord, right_coord, bottom_coord, left_coord), face_encoding in zip(face_locations, face_encodings):	#Going through each face in the frame.
-# 			img_dir = os.path.dirname(os.path.realpath(__file__))+"//Sample_Imgs/" 
+# 			img_dir = os.path.dirname(os.path.realpath(__file__))+"//Sample_Imgs/"
 # 			if cv2.waitKey(1) & 0xFF == ord("y"):	#writes the frame into memory, then closes the window if user presses "y".
-# 				img_dir = os.path.dirname(os.path.realpath("/home/postman/cv_attendance/student_imgs"))
+# 				img_dir = os.path.dirname(os.path.realpath("./student_imgs"))
 # 				cv2.imwrite("{}{}_{}_sample_img.jpg".format(img_dir, sample_name, student_id), frame)
 # 				#print("Sample image captured!")
 # 				img_captured = True
@@ -37,7 +37,7 @@ from os.path import isfile, join
 # 			cv2.rectangle(frame, (left_coord, bottom_coord - 30), (right_coord, bottom_coord), (214, 119, 30), -1)	#a label below the rectangle.
 # 			#cv2.circle(frame, (int((top_coord+bottom_coord)/2),int((left_coord+right_coord)/2)), int((top_coord+bottom_coord)/2), (102, 148, 227), 10)
 # 			cv2.putText(frame, "Face detected - press 'y'", (left_coord + 5, bottom_coord - 5), cv2.FONT_HERSHEY_COMPLEX, 0.75, (255, 255, 255), 2, cv2.LINE_AA, False)
-			
+
 
 # 		cv2.imshow("Taking Samples", frame)	#displays the frame.
 
@@ -46,6 +46,7 @@ from os.path import isfile, join
 
 # 	vid_capture.release()
 # 	cv2.destroyAllWindows()
+
 
 def collect_samples(sample_name, student_id):
     vid_capture = cv2.VideoCapture(0)  # opens the video stream.
@@ -56,25 +57,44 @@ def collect_samples(sample_name, student_id):
             vid_capture.open()  # opens vid stream if vid stream hadn't been opened.
 
         return_val, frame = vid_capture.read()  # captures single frame from camera.
-        rgb_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # OpenCV uses BGR format, whereas face_recognition uses RGB.
+        rgb_img = cv2.cvtColor(
+            frame, cv2.COLOR_BGR2RGB
+        )  # OpenCV uses BGR format, whereas face_recognition uses RGB.
 
         face_locations = face_recognition.face_locations(rgb_img)
         face_encodings = face_recognition.face_encodings(rgb_img, face_locations)
 
-        for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-            img_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Sample_Imgs")
-            
+        for (top, right, bottom, left), face_encoding in zip(
+            face_locations, face_encodings
+        ):
+            img_dir = os.path.join(
+                os.path.dirname(os.path.realpath(__file__)), "Sample_Imgs"
+            )
+
             if cv2.waitKey(1) & 0xFF == ord("y"):
-                img_dir = os.path.join("/home/postman/cv_attendance/student_imgs")
-                img_path = os.path.join(img_dir, "{}_{}_sample_img.jpg".format(sample_name, student_id))
+                img_dir = os.path.join("./student_imgs")
+                img_path = os.path.join(
+                    img_dir, "{}_{}_sample_img.jpg".format(sample_name, student_id)
+                )
                 cv2.imwrite(img_path, frame)
                 img_captured = True
                 continue
 
             cv2.rectangle(frame, (left, top), (right, bottom), (214, 119, 30), 2)
-            cv2.rectangle(frame, (left, bottom - 30), (right, bottom), (214, 119, 30), -1)
-            cv2.putText(frame, "Face detected - press 'y'", (left + 5, bottom - 5),
-                        cv2.FONT_HERSHEY_COMPLEX, 0.75, (255, 255, 255), 2, cv2.LINE_AA, False)
+            cv2.rectangle(
+                frame, (left, bottom - 30), (right, bottom), (214, 119, 30), -1
+            )
+            cv2.putText(
+                frame,
+                "Face detected - press 'y'",
+                (left + 5, bottom - 5),
+                cv2.FONT_HERSHEY_COMPLEX,
+                0.75,
+                (255, 255, 255),
+                2,
+                cv2.LINE_AA,
+                False,
+            )
 
         cv2.imshow("Taking Samples", frame)
 
@@ -85,23 +105,21 @@ def collect_samples(sample_name, student_id):
     cv2.destroyAllWindows()
 
 
-
-
 # def load_images(): #sample_name):	#encodes the sample images as arrays.
 # 	sample_face_encodings, sample_face_names = [], []
 # 	img_found = False
 # 	try:
 # 		img_dir = os.path.dirname(os.path.realpath(__file__))+"/Sample_Imgs/"  	 #image directory.
 # 		onlyfiles = [f for f in listdir(img_dir) if isfile(join(img_dir, f))]
-# 		for i in onlyfiles: 
-			
+# 		for i in onlyfiles:
+
 # 			if i[::-1][:15][::-1] == "_sample_img.jpg":
 # 				img_found = True
 # 				sample_name2 = i.split("_")[0]
 # 				student_id = i.split("_")[1]
 # 				sample_face_names.append(sample_name2+"_"+student_id)
 # 				sample_img2 = face_recognition.load_image_file("{}{}_{}_sample_img.jpg".format(img_dir, sample_name2, student_id))	#loads the image from the file.
-				
+
 # 				try:
 # 					sample_face_encoding2 = face_recognition.face_encodings(sample_img2)[0]
 # 					sample_face_encodings.append(sample_face_encoding2)
@@ -111,34 +129,41 @@ def collect_samples(sample_name, student_id):
 # 		if img_found == False:
 # 			print("No sample images taken!")
 # 			quit()
-	
+
 # 	except Exception as es:
 # 		print("You haven't given us a sample for this name yet!")
 # 		quit()
 
-# 	return sample_face_encodings, sample_face_names 
+# 	return sample_face_encodings, sample_face_names
+
 
 def load_images():
     sample_face_encodings, sample_face_names = [], []
     img_found = False
 
     try:
-        img_dir = "/home/postman/cv_attendance/student_imgs/"  # Student images directory.
+        img_dir = "./student_imgs/"  # Student images directory.
         onlyfiles = [f for f in listdir(img_dir) if isfile(join(img_dir, f))]
-        
+
         for i in onlyfiles:
             if i[::-1][:15][::-1] == "_sample_img.jpg":
                 img_found = True
                 sample_name2 = i.split("_")[0]
                 student_id = i.split("_")[1]
                 sample_face_names.append(sample_name2 + "_" + student_id)
-                sample_img2 = face_recognition.load_image_file("{}{}_{}_sample_img.jpg".format(img_dir, sample_name2, student_id))
+                sample_img2 = face_recognition.load_image_file(
+                    "{}{}_{}_sample_img.jpg".format(img_dir, sample_name2, student_id)
+                )
 
                 try:
-                    sample_face_encoding2 = face_recognition.face_encodings(sample_img2)[0]
+                    sample_face_encoding2 = face_recognition.face_encodings(
+                        sample_img2
+                    )[0]
                     sample_face_encodings.append(sample_face_encoding2)
                 except IndexError:
-                    print("Couldn't detect any faces in {}'s image!".format(sample_name2))
+                    print(
+                        "Couldn't detect any faces in {}'s image!".format(sample_name2)
+                    )
                     continue
 
         if not img_found:
@@ -151,6 +176,7 @@ def load_images():
 
     return sample_face_encodings, sample_face_names
 
+
 # def face_recognition_fn(sample_face_encodings, sample_face_names):
 # 	vid_capture = cv2.VideoCapture(0)
 
@@ -158,7 +184,7 @@ def load_images():
 
 # 	while True:
 # 		return_val, frame = vid_capture.read()	#captures a single frame.
-		
+
 # 		if not vid_capture.isOpened():	#sometimes, vid_capture may not open the vid stream.
 # 			vid_capture.open()	#if it doesn't open the stream, we open it here.
 
@@ -179,7 +205,7 @@ def load_images():
 # 			best_match_ind = np.argmin(face_distances)	#finds min distanced face in the array, 'face_distances'
 # 			if face_matches[best_match_ind]:	#if the index exists in the matches list, we set the name to the corresponding sample name.
 # 				display_name_id = sample_face_names[best_match_ind]
-				
+
 # 				date = datetime.now().strftime("%d-%m-%Y")
 # 				time = datetime.now().strftime("%H:%M:%S")
 # 				identified_faces.append(display_name_id)
@@ -192,8 +218,8 @@ def load_images():
 # 				cv2.rectangle(frame, (left_coord, bottom_coord - 30), (right_coord, bottom_coord), (214, 119, 30), -1)	#a label below the rectangle.
 # 				#cv2.circle(frame, (int((top_coord+bottom_coord)/2),int((left_coord+right_coord)/2)), int((top_coord+bottom_coord)/2), (102, 148, 227), 10)
 # 				cv2.putText(frame, str(display_name_id.split("_")[0]+", Student ID: "+display_name_id.split("_")[1]), (left_coord + 5, bottom_coord - 5), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA, False)
-# 				#Syntax: cv2.putText(<img>, <text>, <origin>, <font>, <fontScale AKA size>, 
-# 				#<colour>, <thickness>, <lineType - cv2.line_AA stands for anti-aliased line, 
+# 				#Syntax: cv2.putText(<img>, <text>, <origin>, <font>, <fontScale AKA size>,
+# 				#<colour>, <thickness>, <lineType - cv2.line_AA stands for anti-aliased line,
 # 				#basically its a smoothly curved line.>, <if text starts from bottom-left - default: False>)
 # 			except:
 # 				pass
@@ -227,14 +253,30 @@ def face_recognition_fn(sample_face_encodings, sample_face_names):
         face_encodings = face_recognition.face_encodings(rgb_img, face_locations)
 
         cv2.rectangle(frame, (20, 15), (340, 55), (214, 119, 30), -1)
-        cv2.putText(frame, str("Press 'q' to close"), (30, 45), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA, False)
+        cv2.putText(
+            frame,
+            str("Press 'q' to close"),
+            (30, 45),
+            cv2.FONT_HERSHEY_COMPLEX,
+            1,
+            (255, 255, 255),
+            2,
+            cv2.LINE_AA,
+            False,
+        )
 
-        for (top_coord, right_coord, bottom_coord, left_coord), face_encoding in zip(face_locations, face_encodings):
-            face_matches = face_recognition.compare_faces(sample_face_encodings, face_encoding)
+        for (top_coord, right_coord, bottom_coord, left_coord), face_encoding in zip(
+            face_locations, face_encodings
+        ):
+            face_matches = face_recognition.compare_faces(
+                sample_face_encodings, face_encoding
+            )
 
             display_name_id = "Unknown"
 
-            face_distances = face_recognition.face_distance(sample_face_encodings, face_encoding)
+            face_distances = face_recognition.face_distance(
+                sample_face_encodings, face_encoding
+            )
             best_match_ind = np.argmin(face_distances)
             if face_matches[best_match_ind]:
                 display_name_id = sample_face_names[best_match_ind]
@@ -244,13 +286,43 @@ def face_recognition_fn(sample_face_encodings, sample_face_names):
                 identified_faces.append(display_name_id)
 
                 # Assuming mark_attendance is a function you have defined elsewhere
-                mark_attendance(date, time, display_name_id.split("_")[0], display_name_id.split("_")[1])
+                mark_attendance(
+                    date,
+                    time,
+                    display_name_id.split("_")[0],
+                    display_name_id.split("_")[1],
+                )
 
             try:
-                cv2.rectangle(frame, (left_coord, top_coord), (right_coord, bottom_coord), (214, 119, 30), 2)
-                cv2.rectangle(frame, (left_coord, bottom_coord - 30), (right_coord, bottom_coord), (214, 119, 30), -1)
-                cv2.putText(frame, str(display_name_id.split("_")[0] + ", Student ID: " + display_name_id.split("_")[1]),
-                            (left_coord + 5, bottom_coord - 5), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA, False)
+                cv2.rectangle(
+                    frame,
+                    (left_coord, top_coord),
+                    (right_coord, bottom_coord),
+                    (214, 119, 30),
+                    2,
+                )
+                cv2.rectangle(
+                    frame,
+                    (left_coord, bottom_coord - 30),
+                    (right_coord, bottom_coord),
+                    (214, 119, 30),
+                    -1,
+                )
+                cv2.putText(
+                    frame,
+                    str(
+                        display_name_id.split("_")[0]
+                        + ", Student ID: "
+                        + display_name_id.split("_")[1]
+                    ),
+                    (left_coord + 5, bottom_coord - 5),
+                    cv2.FONT_HERSHEY_COMPLEX,
+                    1,
+                    (255, 255, 255),
+                    2,
+                    cv2.LINE_AA,
+                    False,
+                )
             except Exception as e:
                 print("Error:", e)
 
@@ -265,34 +337,59 @@ def face_recognition_fn(sample_face_encodings, sample_face_names):
 
 
 def mark_attendance(date, time, name, student_id):
-	csv_input = []
-	student_id_date = []
-	while True:
-		try:
-			with open(os.path.dirname(os.path.realpath(__file__))+"/Attendance_register_25_7_21.csv", "r", newline = "") as file:
-				reader_object = csv.reader(file)
-				for i in reader_object:
-					csv_input.append(i)
-					
-					if i != ["\n"] and i != ["Student ID", "Name", "Date", "Time", "Present/Absent"]:
-						student_id_date.append([i[0], i[2]])
-			break
-		except FileNotFoundError:
-			with open(os.path.dirname(os.path.realpath(__file__))+"/Attendance_register_25_7_21.csv", "w", newline = "") as file:
-				writer_object = csv.writer(file)
-				title = ["Student ID", "Name", "Date", "Time", "Present/Absent"]
-				writer_object.writerow(title)
+    csv_input = []
+    student_id_date = []
+    while True:
+        try:
+            with open(
+                os.path.dirname(os.path.realpath(__file__))
+                + "/Attendance_register_25_7_21.csv",
+                "r",
+                newline="",
+            ) as file:
+                reader_object = csv.reader(file)
+                for i in reader_object:
+                    csv_input.append(i)
 
+                    if i != ["\n"] and i != [
+                        "Student ID",
+                        "Name",
+                        "Date",
+                        "Time",
+                        "Present/Absent",
+                    ]:
+                        student_id_date.append([i[0], i[2]])
+            break
+        except FileNotFoundError:
+            with open(
+                os.path.dirname(os.path.realpath(__file__))
+                + "/Attendance_register_25_7_21.csv",
+                "w",
+                newline="",
+            ) as file:
+                writer_object = csv.writer(file)
+                title = ["Student ID", "Name", "Date", "Time", "Present/Absent"]
+                writer_object.writerow(title)
 
-	with open(os.path.dirname(os.path.realpath(__file__))+"/Attendance_register_25_7_21.csv", "w", newline = "") as file:
-		writer_object = csv.writer(file)
-		title = ["Student ID", "Name", "Date", "Time", "Present/Absent"]
-		if title not in csv_input:
-			csv_input.append(title)
-		
-		if student_id_date != [] and student_id_date[len(student_id_date)-1][1] != date and student_id_date[len(student_id_date)-1][1] != "" and csv_input[len(csv_input)-1] != ["\n"]:
-			csv_input.append("\n")
-		
-		if [student_id, date] not in student_id_date:
-			csv_input.append([student_id, name, date, time, "Present"])
-		writer_object.writerows(csv_input)
+    with open(
+        os.path.dirname(os.path.realpath(__file__))
+        + "/Attendance_register_25_7_21.csv",
+        "w",
+        newline="",
+    ) as file:
+        writer_object = csv.writer(file)
+        title = ["Student ID", "Name", "Date", "Time", "Present/Absent"]
+        if title not in csv_input:
+            csv_input.append(title)
+
+        if (
+            student_id_date != []
+            and student_id_date[len(student_id_date) - 1][1] != date
+            and student_id_date[len(student_id_date) - 1][1] != ""
+            and csv_input[len(csv_input) - 1] != ["\n"]
+        ):
+            csv_input.append("\n")
+
+        if [student_id, date] not in student_id_date:
+            csv_input.append([student_id, name, date, time, "Present"])
+        writer_object.writerows(csv_input)
